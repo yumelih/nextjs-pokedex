4075a2e08603c1bebb1b04ad7e3f9dcfd7a9c216
yumelih <yumelih@gmail.com>
2024-01-11 15:33:43 +0300

    small time change

--- src/network/pokemon-api.ts
+++ src/network/pokemon-api.ts
@@ -2,8 +2,8 @@ import { Pokemon, PokemonPage } from "@/models/Pokemon";
 import api from "./axiosInstance";
 
 export async function getPokemon(name: string) {
-  const delay = Math.random() * 2000;
-  await new Promise((r) => setTimeout(r, delay));
+  // const delay = Math.random() * 2000;
+  // await new Promise((r) => setTimeout(r, delay));
 
   // Pokemon is the return type.
   // api.get because it is GET request.
